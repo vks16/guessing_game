41dd26ab8ae8ef642a62d7081a108382a41f7e0a
vks16 <vishalsharma16171718@gmail.com>
2020-06-10 20:42:36 +0530

    Rust Guess v 0.1

---
+++ src/main.rs
@@ -0,0 +1,37 @@
+extern crate rand;
+
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
+
+fn main() {
+    println!("Guess the number between 1 to 100.!");
+
+	let secret_number = rand::thread_rng().gen_range(1,101);
+
+	loop {
+		println!("Please input your guess.");
+
+		let mut guess = String::new();
+
+		io::stdin().read_line(&mut guess)
+			.expect("Failed to read");
+
+		let guess: u32 = match guess.trim().parse(){
+							Ok(num) => num,
+							Err(_) => {
+								println!("Enter only a valid number please!\n");
+								continue;
+							},
+						};
+
+		match guess.cmp(&secret_number) {
+			Ordering::Less => println!("Too Small"),
+			Ordering::Greater => println!("Too Big"),
+			Ordering::Equal => {
+				println!("You win");
+				break;
+			},
+		}
+	}
+}
